f5932014f9aa8f1f8179b87a64a3aa9147306a5c
Michael Jurke <m.jurke@gmx.de>
2023-02-23 18:31:21 +0100

    Implement domain filters for custom conditions on report notes for sale orders, stock pickings and account moves

--- models/account_move.py
+++ models/account_move.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import api, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def get_custom_report_notes(self, field, position):
-        return self.env["custom_report_notes.note"].search(
+        self.ensure_one()
+
+        all_notes = self.env["custom_report_notes.note"].search(
             self._custom_report_notes_domain(field, position)
         )
+        matching_notes = self.env["custom_report_notes.note"]
+
+        for note in all_notes:
+
+            if not note.account_move_domain:
+                continue
+
+            domain = note._parse_account_move_domain()
+
+            if not domain:
+                continue
+
+            domain.append(("id", "=", self.id))
+
+            if self.env['account.move'].search(domain, limit=1):
+                matching_notes |= note
+
+        return matching_notes
 
     def _custom_report_notes_domain(self, field, position):
         domain = [
@@ -27,3 +47,19 @@ class AccountMove(models.Model):
         return self.env.ref("custom_report_notes.account_move_state_{}".format(
             self.state
         )).id
+
+    @api.model
+    def search_count(self, args):
+        relation_tuples = self.env.context.get(
+            "preview_custom_report_note_account_move_state_ids"
+        )
+        if relation_tuples:
+            state_ids = [
+                _id for relation_tuple in relation_tuples for _id in relation_tuple[2]
+                if relation_tuple[2]
+            ]
+            if state_ids:
+                args = args + self.env[
+                    "custom_report_notes.note"
+                ]._get_base_account_move_domain(state_ids)
+        return super().search_count(args)
--- models/note.py
+++ models/note.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
+from ast import literal_eval
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class CustomReportNote(models.Model):
@@ -41,26 +45,106 @@ class CustomReportNote(models.Model):
 
     page_break = fields.Boolean("Page Break", default=False)
 
+    content = fields.Html("Content", translate=True, required=True)
+
+    # Sales Order Filters
+
     applicable_to_sale_orders = fields.Boolean(
         "Applicable To Sales Orders",
         default=True,
     )
+
     sale_order_state_ids = fields.Many2many(
         string="Sales Order States",
         comodel_name="custom_report_notes.report_state",
         relation="custom_report_notes_note_sale_order_state_rel",
         domain=[('res_model', '=', 'sale.order')],
     )
+
+    @api.model
+    def _get_base_sale_order_domain(self, state_ids):
+        states = self.env["custom_report_notes.report_state"].browse(state_ids)
+        domain = [("state", "in", states.with_context(lang=False).mapped('name'))]
+        return domain
+
+    def _get_default_sale_order_domain(self):
+        return []
+
+    def _compute_sale_order_domain(self):
+        for record in self:
+            record.sale_order_domain = repr(record._get_default_sale_order_domain())
+
+    def _parse_sale_order_domain(self):
+        self.ensure_one()
+        try:
+            domain = literal_eval(self.sale_order_domain)
+        except Exception as e:
+            _logger.warning(
+                "Invalid sale.order domain for report note #{}: {}".format(
+                    self.id, str(e)
+                )
+            )
+            domain = []
+        return domain
+
+    sale_order_domain = fields.Char(
+        string="Sales Order Domain",
+        compute=_compute_sale_order_domain,
+        readonly=False,
+        store=True,
+    )
+
+    # Stock Picking Filters
+
     applicable_to_stock_pickings = fields.Boolean(
         "Applicable To Deliveries",
         default=True,
     )
+
     stock_picking_state_ids = fields.Many2many(
         string="Delivery States",
         comodel_name="custom_report_notes.report_state",
         relation="custom_report_notes_note_stock_picking_state_rel",
         domain=[('res_model', '=', 'stock.picking')],
     )
+
+    @api.model
+    def _get_base_stock_picking_domain(self, state_ids):
+        states = self.env["custom_report_notes.report_state"].browse(state_ids)
+        domain = [("state", "in", states.with_context(lang=False).mapped('name'))]
+        return domain
+
+    def _get_default_stock_picking_domain(self):
+        return []
+
+    def _compute_stock_picking_domain(self):
+        for record in self:
+            record.stock_picking_domain = repr(
+                record._get_default_stock_picking_domain()
+            )
+
+    def _parse_stock_picking_domain(self):
+        self.ensure_one()
+        try:
+            domain = literal_eval(self.stock_picking_domain)
+        except Exception as e:
+            _logger.warning(
+                "Invalid stock.picking domain for report note #{}: {}".format(
+                    self.id, str(e)
+                )
+            )
+            domain = []
+        return domain
+
+    stock_picking_domain = fields.Char(
+        string="Stock Picking Domain",
+        compute=_compute_stock_picking_domain,
+        readonly=False,
+        store=True,
+    )
+
+    # Account Moves Filters
+
     applicable_to_account_moves = fields.Boolean(
         "Applicable To Invoices",
         default=True,
@@ -72,7 +156,42 @@ class CustomReportNote(models.Model):
         domain=[('res_model', '=', 'account.move')],
     )
 
-    content = fields.Html("Content", translate=True, required=True)
+    @api.model
+    def _get_base_account_move_domain(self, state_ids):
+        states = self.env["custom_report_notes.report_state"].browse(state_ids)
+        domain = [("state", "in", states.with_context(lang=False).mapped('name'))]
+        return domain
+
+    def _get_default_account_move_domain(self):
+        return []
+
+    def _compute_account_move_domain(self):
+        for record in self:
+            record.account_move_domain = repr(
+                record._get_default_account_move_domain()
+            )
+
+    def _parse_account_move_domain(self):
+        self.ensure_one()
+        try:
+            domain = literal_eval(self.account_move_domain)
+        except Exception as e:
+            _logger.warning(
+                "Invalid account.move domain for report note #{}: {}".format(
+                    self.id, str(e)
+                )
+            )
+            domain = []
+        return domain
+
+    account_move_domain = fields.Char(
+        string="Account Move Domain",
+        compute=_compute_account_move_domain,
+        readonly=False,
+        store=True,
+    )
+
+    # helper methods
 
     def _get_combinations(self):
         positions = list(
